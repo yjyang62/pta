f8844be1dfd83cd897fc100edc7ea746ea1283ca
yangjinyang <yangjinyang5@huawei.com>
2026-03-25 16:44:07 +0800

    修改计数器
    Signed-off-by: yangjinyang <yangjinyang5@huawei.com>

--- torch_npu/csrc/core/npu/NPUCachingAllocator.cpp
+++ torch_npu/csrc/core/npu/NPUCachingAllocator.cpp
@@ -1526,8 +1526,8 @@ public:
         // done outside the lock because we don't know what locks the recorder needs
         // to have...
         g_malloc_call_count++;
-        if (g_malloc_call_count == 10000) {
-            TORCH_CHECK_WITH(OutOfMemoryError, false, "Simulated OutOfMemoryError: malloc call count exceeded 10000");
+        if (g_malloc_call_count == 30000) {
+            TORCH_CHECK_WITH(OutOfMemoryError, false, "Simulated OutOfMemoryError: malloc call count exceeded 30000");
         }
         auto context = maybeGatherContext(RecordContext::STATE);
 
