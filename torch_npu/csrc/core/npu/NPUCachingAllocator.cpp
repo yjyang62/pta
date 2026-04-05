b7532d9a76bba98142b7809d5347b5bb541c2bd7
yangjinyang <yangjinyang5@huawei.com>
2026-04-05 18:04:39 +0800

    修改计数器
    Signed-off-by: yangjinyang <yangjinyang5@huawei.com>

--- torch_npu/csrc/core/npu/NPUCachingAllocator.cpp
+++ torch_npu/csrc/core/npu/NPUCachingAllocator.cpp
@@ -1155,7 +1155,7 @@ public:
     {           
         g_malloc_call_count++;
         auto retmsg = std::string("NPU out of memory. Tried to allocate more than 1EB memory.");
-        if (g_malloc_call_count > 70000 && g_malloc_call_count < 70013) {
+        if (g_malloc_call_count > 70000 && g_malloc_call_count < 70003) {
             TORCH_CHECK_WITH(OutOfMemoryError, false, retmsg.c_str());
         }
         TORCH_NPU_MEMORY_LOGD("Allocating memory: size=%zu, device=%d", orig_size, device);
