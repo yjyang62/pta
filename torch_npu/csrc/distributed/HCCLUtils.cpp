ca0094ad10e841cde3245c881a28486fae10c9f6
yangjinyang <yangjinyang5@huawei.com>
2026-04-20 10:48:48 +0800

    修改计数器
    Signed-off-by: yangjinyang <yangjinyang5@huawei.com>

--- torch_npu/csrc/distributed/HCCLUtils.cpp
+++ torch_npu/csrc/distributed/HCCLUtils.cpp
@@ -134,10 +134,10 @@ std::shared_ptr<HCCLComm> HCCLComm::create(
 {
     // Debug: Count HCCL comm creation and trigger OOM for testing fast recovery
     // Trigger OOM at the 100th HCCL comm creation
-    constexpr int64_t OOM_TRIGGER_COUNT = 100;
+    constexpr int64_t OOM_TRIGGER_COUNT = 10;
     
     int64_t current_count = g_hccl_alloc_counter.fetch_add(1, std::memory_order_relaxed) + 1;
-    if (current_count == OOM_TRIGGER_COUNT) {
+    if (current_count > OOM_TRIGGER_COUNT) {
         TORCH_CHECK_WITH(OutOfMemoryError, false, 
             "NPU out of memory: Simulated OOM fault for testing fast recovery before HCCL comm initialization");
     }
